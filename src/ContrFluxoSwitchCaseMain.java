dcc9a01a08678e4061dab4838ee3552ef9308d6a
BETHA\bruno.kurzawe <compras@4>
2022-07-26 22:43:46 -0300

    Controle de Switch

---
+++ src/ContrFluxoSwitchCaseMain.java
@@ -0,0 +1,17 @@
+public class ContrFluxoSwitchCaseMain {
+    public static void main(String[] args) {
+
+        char letra = 'C';
+
+        switch(letra) {
+            case 'A' :
+                System.out.println("A");
+                break;
+            case 'B' :
+                System.out.println("B");
+                break;
+            default:
+                System.out.println("?");
+        }
+    }
+}
