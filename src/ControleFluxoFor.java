15ac267d2fcc35a10a7b83b14b000aeb22e7ea46
BETHA\bruno.kurzawe <compras@4>
2022-07-26 22:33:17 -0300

    Controle de Fluxo while

---
+++ src/ControleFluxoFor.java
@@ -0,0 +1,7 @@
+public class ControleFluxoFor {
+    public static void main(String[] args) {
+        for ( int x = 0; x < 10; x++ ) {
+            System.out.println ("item " + x);
+        }
+    }
+}
